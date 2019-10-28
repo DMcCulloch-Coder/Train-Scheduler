33c4693d384e8eed32bb7e8ca40d648bf45e61bd
David <DMcCulloch86@gmail.com>
2019-10-28 11:58:49 -0400

    initial commit, added links to project

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyCVEbsGelDPYhjqrfMF1GJLEHoi6TWaDSY",
+        authDomain: "train-schedule-35894.firebaseapp.com",
+        databaseURL: "https://train-schedule-35894.firebaseio.com",
+        projectId: "train-schedule-35894",
+        storageBucket: "train-schedule-35894.appspot.com",
+        messagingSenderId: "392629642116",
+        appId: "1:392629642116:web:6433a2dfc9c52c5cda73d1"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    let database = firebase.database();
+
+    
+
+
+
+
+
+
+
+
+
+})
